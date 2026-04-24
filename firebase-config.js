49293a34c4e59bb6e555b1f97f652b0d4c1e0ef9
evertonsantos17-blip <everton_santos.17@outlook.com>
2026-04-24 16:49:22 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCVkIPTs0h3e1qLeXg1CR6jSEMwAUAou9U",
+  authDomain: "trip-planner-906e6.firebaseapp.com",
+  projectId: "trip-planner-906e6",
+  storageBucket: "trip-planner-906e6.firebasestorage.app",
+  messagingSenderId: "1088738113837",
+  appId: "1:1088738113837:web:4581faf5e773b7ea84bba4"
+};
